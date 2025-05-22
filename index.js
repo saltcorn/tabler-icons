a49231dc144fa0f9d4072ebe60461cd5f1ad45be
gerard1001 <rutayisiregerard2002@gmail.com>
2025-05-22 06:25:22 +0200

    tabler icons - initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const Workflow = require("@saltcorn/data/models/workflow");
+const Form = require("@saltcorn/data/models/form");
+const icons = require("./tabler-icons.json");
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  plugin_name: "tabler-icons",
+  headers: [
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-200-filled.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-200-outline.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-200.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-300-filled.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-300-outline.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-300.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-filled.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons-outline.css",
+    },
+    {
+      css: "/plugins/public/tabler-icons/tabler-icons.css",
+    },
+  ],
+  icons: icons.map((icon) => `ti ti-${icon}`),
+  configuration_workflow: () =>
+    new Workflow({
+      steps: [
+        {
+          name: "Tabler Icon configuration",
+          form: () =>
+            new Form({
+              fields: [
+                {
+                  name: "enabled_icons",
+                  label: "Enabled Icons",
+                  type: "String",
+                  sublabel:
+                    "Comma-separated list of icons to enable. If not provided, all icons will be enabled.",
+                  required: false,
+                  attributes: {
+                    options: icons.join(", "),
+                  },
+                },
+                {
+                  name: "css_path",
+                  label: "Custom CSS Path",
+                  type: "String",
+                  sublabel:
+                    "Path to custom CSS file. If not provided, the default Tabler Icons CSS will be used.",
+                  required: false,
+                },
+              ],
+            }),
+        },
+      ],
+    }),
+
+  onLoad: async (config) => {
+    if (config.enabled_icons) {
+      module.exports.icons = config.enabled_icons
+        .split(",")
+        .map((icon) => `ti ti-${icon.trim()}`);
+    }
+    if (config.css_path) {
+      headers.push({
+        css: config.css_path,
+      });
+      module.exports.headers = headers;
+    }
+  },
+};
